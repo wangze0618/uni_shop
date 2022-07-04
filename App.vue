39c390b92b707844e3a330d1d49570ba5fcd72d7
wangze0618 <2867273828@qq.com>
2022-07-04 15:40:26 +0800

    tabbat

--- App.vue
+++ App.vue
@@ -15,9 +15,9 @@
 
 <style lang="scss">
 	/*每个页面公共css */
-	@import '@/uni_modules/uni-scss/index.scss';
+	// @import '@/uni_modules/uni-scss/index.scss';
 	/* #ifndef APP-NVUE */
-	@import '@/static/customicons.css';
+	// @import '@/static/customicons.css';
 	// 设置整个项目的背景色
 	page {
 		background-color: #f5f5f5;
