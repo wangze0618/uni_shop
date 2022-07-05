63e917e89d8704ba437fda9853104212e2d011a7
wangze0618 <2867273828@qq.com>
2022-07-05 23:25:31 +0800

    home

---
+++ api/home.js
@@ -0,0 +1,16 @@
+import request from '@/utils/request.js'
+
+// 获取轮播图数据
+export const getSwiperArray = () => {
+	return request('/api/public/v1/home/swiperdata', 'get')
+}
+
+// 获取分类列表数据
+export const getNavListData = () => {
+	return request('/api/public/v1/home/catitems', 'get')
+}
+
+// 获取楼层数据
+export const getFloorData = () => {
+	return request('/api/public/v1/home/floordata', 'get')
+}
