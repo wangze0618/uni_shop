e716402ab39280bab381be50de600ee3f6ec7f2d
wangze0618 <2867273828@qq.com>
2022-07-07 01:00:11 +0800

    322 finished

---
+++ api/search.js
@@ -0,0 +1,6 @@
+import request from '@/utils/request.js'
+
+// 搜索建议查询
+export const getSearchListData = (query) => {
+	return request(`/api/public/v1/goods/qsearch?query=${query}`)
+}
--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -1,18 +1,29 @@
 <template>
+	<!-- 顶部搜索栏 -->
 	<view class="search-box">
 		<uni-search-bar :focus="true" class="search-bar" maxlength="12" placeholder="搜索" radius="100" @input="input($event)" clearButton="auto" v-model="searchText" />
+	</view>
+	<!-- 搜索列表展示区 -->
+	<view class="search-list">
+		<view class="search-list-item" v-for="(item, index) in searchList" :key="index">
+			<view class="goods-name">{{ index + 1 }}.&nbsp;{{ item.goods_name }}</view>
+			<uni-icons type="arrowright" size="16"></uni-icons>
+		</view>
 	</view>
 </template>
 
 <script>
+import { getSearchListData } from '@/api/search.js';
 export default {
 	data() {
 		return {
 			searchText: '',
-			timer: null
+			timer: null,
+			searchList: []
 		};
 	},
 	methods: {
+		// 输入事件
 		input(e) {
 			// 防抖处理
 			if (this.timer) {
@@ -21,9 +32,18 @@ export default {
 			}
 			this.timer = setTimeout(() => {
 				// 防止空值 赋值给 searchText
-				if (e == !'') this.searchText = e;
-				console.log(this.searchText);
+				if (e !== '') this.searchText = e;
+				this.getSearchList(this.searchText);
 			}, 1000);
+		},
+		// 获取 搜索关键词列表
+		async getSearchList(params) {
+			try {
+				const { data } = await getSearchListData(params);
+				this.searchList = data.message;
+			} catch (e) {
+				uni.$showMsg();
+			}
 		}
 	}
 };
@@ -38,5 +58,23 @@ export default {
 	top: 0;
 	z-index: 999;
 	background-color: #c00000;
+}
+.search-list {
+	padding: 0 10rpx;
+	.search-list-item {
+		font-size: 24rpx;
+		padding: 26rpx 0;
+		border-bottom: 2rpx solid #e8e8e8;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		.goods-name {
+			margin-left: 6rpx;
+			white-space: nowrap;
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
+	}
 }
 </style>
